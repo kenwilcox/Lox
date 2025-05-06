c632a62d3711b66fe1c9d32aff633d7b02b6c095
Ken Wilcox <wilcoxk@gmail.com>
2025-05-05 19:59:49 -0600

    adding value and vm

---
+++ c/clox/vm.c
@@ -0,0 +1,54 @@
+//
+//  vm.c
+//  clox
+//
+//  Created by Ken Wilcox on 5/5/25.
+//
+
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+VM vm;
+
+void initVM(void) {
+
+}
+
+void freeVM(void) {
+
+}
+
+static InterpretResult run(void) {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for(;;) {
+
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN:
+                return INTERPRET_OK;
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
---
+++ c/clox/vm.h
@@ -0,0 +1,28 @@
+//
+//  vm.h
+//  clox
+//
+//  Created by Ken Wilcox on 5/5/25.
+//
+
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM(void);
+void freeVM(void);
+InterpretResult interpret(Chunk* chunk);
+
+#endif
