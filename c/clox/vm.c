d0763438d6d99f5574d4f2e66c50b2cf36c5c15e
Ken Wilcox <wilcoxk@gmail.com>
2025-05-13 17:48:44 -0600

    added binary operations

--- c/clox/vm.c
+++ c/clox/vm.c
@@ -13,38 +13,72 @@
 
 VM vm;
 
-void initVM(void) {
+static void resetStack(void) {
+    vm.stackTop = vm.stack;
+}
 
+void initVM(void) {
+    resetStack();
 }
 
 void freeVM(void) {
 
 }
 
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop(void) {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
 static InterpretResult run(void) {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
     for(;;) {
 
 #ifdef DEBUG_TRACE_EXECUTION
-    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
         }
     }
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
