b1564756710886c528c43bd292c0628053af5934
Ken Wilcox <wilcoxk@gmail.com>
2025-05-22 00:50:33 -0600

    added strings

--- c/clox/compiler.c
+++ c/clox/compiler.c
@@ -176,6 +176,12 @@ static void number(void) {
     emitConstant(NUMBER_VAL(value));
 }
 
+static void string(void) {
+    // +1 and -2 remove the quotes from the string
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
+                                  parser.previous.length - 2)));
+}
+
 static void unary(void) {
     TokenType operatorType = parser.previous.type;
 
@@ -211,7 +217,7 @@ ParseRule rules[] = {
     [TOKEN_LESS]          = {NULL,     binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]    = {NULL,     binary, PREC_COMPARISON},
     [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_STRING]        = {string,   NULL,   PREC_NONE},
     [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
     [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
